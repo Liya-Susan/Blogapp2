b2b5e3c7ebfdde7f57918629f110cb9d1f0a5a5d
Liya Susan Biju <liyasusanbiju@gmail.com>
2022-07-28 10:57:00 +0530

    first commit

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
-import express from "express";
-import mongoose from "mongoose";
-import blogRouter from "./routes/blog-routes";
-import router from "./routes/user-routes";
-import cors from 'cors';
+const express = require('express')
+const cors = require('cors');
+const { default: mongoose } = require('mongoose');
+
+const blogRouter  = require('./routes/blog-routes')
+const router  = require('./routes/user-routes')
+
+require('dotenv').config();
 
 const app=express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json())
+
+const url = process.env.ATLAS_URL;
+
+mongoose.connect(url,{useUnifiedTopology:true},(err)=>console.log(console.log("connection created",err)))
+
 app.use("/api/user",router);
 app.use("/api/blog",blogRouter);
 
 
-mongoose.connect(
-    "mongodb+srv://adminn:Qw5bc7SpWYJAngNl@cluster0.wcjoh.mongodb.net/?retryWrites=true&w=majority"
-).then(()=>app.listen(5002)
-).then(()=>console.log('connected to database')
-).catch((err)=>console.log(err));
+const port = process.env.PORT
+
+app.listen(port,() =>{
+    console.log('server is running on port:',port)
+})
+
+
